f4614c21d09d66717971d7b85c1737b999a8b8d4
tiWebCoder <127739121+tiWebCoder@users.noreply.github.com>
2023-10-13 15:23:45 +0100

    Add files via upload

---
+++ dartsScript.js
@@ -0,0 +1,13 @@
+/**
+ * Opens the menu when the icon is clicked
+ */
+function openMenu(){
+    document.getElementById('menu').style.transform = 'scaleY(1)';
+}
+
+/**
+ * Closes the menu when the close icon is clicked
+ */
+function closeMenu(){
+    document.getElementById('menu').style.transform = 'scaleY(0)';
+}
